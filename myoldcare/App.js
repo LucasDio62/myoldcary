ee1af89a22f739375d2dcae4b74871bf57819fec
LucasDio62 <lucasadriel007@gmail.com>
2024-11-18 17:00:07 -0400

    projeto testado e apresentado(falta funcionalidade e conrreçao de bugs)

--- myoldcare/App.js
+++ myoldcare/App.js
@@ -1,6 +1,6 @@
-import React, {useRef, useState} from 'react';
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createStackNavigator } from '@react-navigation/stack';
+import React, {useRef, useState, useEffect } from 'react';
+import DateTimePicker from '@react-native-community/datetimepicker';
+
 import {
   Button,
   Pressable,
@@ -9,15 +9,16 @@ import {
   Text,
   StyleSheet,
   View,
+  Alert,
   Dimensions,
+  TextInput,
+  ScrollView,
+  TouchableOpacity,
+  Platform,
   Image
 } from 'react-native';
 
-const teste = () =>{
-  console.log("hi");
-}
-teste()
-const { width } = Dimensions.get('window');
+const { width, height } = Dimensions.get('window');
 const App = () => {
 //Valores dos componentes, se for true e aparece se for false ele nao aparece
   const [lembrete, setLembrete] = useState(true);
@@ -30,7 +31,7 @@ const [imgLembrete, setImgLembrete] = useState(require('./assets/bt-lembretes.pn
 const [imgLoja, setImgLoja] = useState(require('./assets/bt-01-removebg-preview.png'));
 const [imgSininho, setImgSininho] = useState(require('./assets/Screenshot_5-removebg-preview.png'));
 const [imgEngrenagem, setImgEngrenagem] = useState(require('./assets/Screenshot_6-removebg-preview (1).png'));
-  
+
   const esconderLembrete = () => {
     //setando o caminho novo dos componentes para trocar a imagem
     setImgLembrete(require('./assets/bt-lembretes.png'));
@@ -90,38 +91,163 @@ const [imgEngrenagem, setImgEngrenagem] = useState(require('./assets/Screenshot_
     setEngrenagem(false);
     setMais(true);
   };
+  
+  const [alarmTime, setAlarmTime] = useState(new Date());
+	const [showTimePicker, setShowTimePicker] = useState(false);
+  const [text, setText] = useState('');
+  const [alarmes, setAlarmes] = useState([
+    { nome_alarme: "lucas", horas: 10, minutos: 50, index: 0 },
+    { nome_alarme: "dorflex", horas: 11, minutos: 50, index: 1 },
+    { nome_alarme: "remedio", horas: 10, minutos: 50, index: 2 },
+    { nome_alarme: "buscopan", horas: 10, minutos: 42, index: 3 },
+  ])
+
+  const verificarAlarmeExistente = (nome) => {
+    return alarmes.some(alarme => alarme.nome_alarme === nome);
+  };
+  const verificarAlarme = (horas, minutos) => {
+    return alarmes.some(alarme => alarme.horas === horas && alarme.minutos === minutos);
+  };
+
+  const adicionar_alarme = () =>{
+    console.log(alarmes)
+    var nome_alarme = text.toLowerCase()
+    if(verificarAlarmeExistente(nome_alarme)){
+      alert("Ja existe um alarme com esse nome!")
+      return
+    }
+    if(nome_alarme == ''){
+      alert("É preciso adicionar um nome para o alarme!")
+      return
+    }
+    var noveo_alarme = [...alarmes]
+    var alarme = {
+      nome_alarme: nome_alarme,
+      horas: alarmTime.getHours(), 
+      minutos: alarmTime.getMinutes(),
+      index: (alarmes[alarmes.length - 1].index +1)
+    }
+    noveo_alarme.push(alarme)
+    setAlarmes(noveo_alarme)
+    alert("Alarme adicionado!")
+    setText('')
+  }
+
+  const showTimePickerModal = () => {
+		setShowTimePicker(true);
+	};
+
+	const hideTimePickerModal = () => {
+		setShowTimePicker(false);
+	};
+
+	const handleTimeChange = (event, selectedTime) => {
+		hideTimePickerModal();
+		if (selectedTime) {
+			setAlarmTime(selectedTime);
+      // console.log(selectedTime.getHours())
+		}
+	};
+
+		const checkAlarm = setInterval(() => {
+			const currentTime = new Date();
+      // console.log(currentTime.getHours())
+
+			if (
+        verificarAlarme(currentTime.getHours(), currentTime.getMinutes()) && (currentTime.getSeconds() >= 0 && currentTime.getSeconds() <= 3)
+			) {
+				// Matched the set alarm time, show an alert
+				Alert.alert("Alarm", "It is time!");
+				// Stop checking once the alert is shown
+				clearInterval(checkAlarm); 
+			}
+		}, 1000); // Check every second
+		// Cleanup on component unmount
 
   return (
     <View style={styles.container}>
         {lembrete && (<View style={[styles.box, { width: width * 0.9 }]}>
-          <Text style={[styles.boxText]}>Este é o componente lembretes</Text>
+
+          <View style={styles.parent}>
+          <ScrollView style={styles.box1}>
+          {alarmes.map((alarme, index) => (
+           index !==0 && (<View key={index} style={styles.rectangle}>
+            <Text style={styles.text_alarm}>{alarme.nome_alarme.toUpperCase(0)}</Text>
+            <Text style={styles.text_alarm_hora}>{alarme.horas}:{alarme.minutos}</Text>
+            </View>
+            )
+        ))}
+          </ScrollView>
+        </View>
         </View>)}
         {loja && (<View style={[styles.box, { width: width * 0.9 }]}>
           <Text style={[styles.boxText]}>Este é o componente loja</Text>
+          
         </View>)}
         {sininho && (<View style={[styles.box, { width: width * 0.9 }]}>
           <Text style={[styles.boxText]}>Este é o componente sininho</Text>
         </View>)}
         {/* componente engrenagem: */}
         {engrenagem && (<View style={[styles.box, { width: width * 0.9 }]}>
-          <Pressable style={styles.button} onPress={() => handlePress('Notificações')}>
+          <Pressable style={styles.button}>
             <Text style={styles.buttonText}>Notificações</Text>
           </Pressable>
 
-          <Pressable style={styles.button} onPress={() => handlePress('Ajustes e Acessibilidade')}>
+          <Pressable style={styles.button}>
             <Text style={styles.buttonText}>Ajustes e Acessibilidade</Text>
           </Pressable>
 
-          <Pressable style={styles.button} onPress={() => handlePress('Idiomas')}>
+          <Pressable style={styles.button}>
             <Text style={styles.buttonText}>Idiomas</Text>
           </Pressable>
 
-          <Pressable style={styles.button} onPress={() => handlePress('Ajuda e Suporte')}>
+          <Pressable style={styles.button}>
             <Text style={styles.buttonText}>Ajuda e Suporte</Text>
           </Pressable>
         </View>)}
-        {mais && (<View style={[styles.box, { width: width * 0.9 }]}>
-          <Text style={[styles.boxText]}>Este é o componente mais</Text>
+        {/* maaaaaaaaaaiiiiissss */}
+        {mais && (<View style={[styles.box_mais, { width: width * 0.9 }]}>
+          <View style={styles.header}>
+			</View>
+
+			<View style={styles.clockContainer}>
+				<Text style={styles.clockText} onPress={showTimePickerModal}>
+					{alarmTime.toLocaleTimeString([], {
+            hour: "2-digit",
+						minute: "2-digit",
+					})}
+				</Text>
+			</View>
+          
+			{showTimePicker && (
+				<DateTimePicker
+					value={alarmTime}
+					mode="time"
+					is24Hour={true}
+					display="spinner"
+					onChange={handleTimeChange}
+				/>
+			)}
+
+      <View style={styles.button_input}>
+      <Text style={styles.text_input}>Digite o nome do alarme</Text>
+      
+      {/* Campo de texto */}
+      <TextInput
+        style={styles.input}  // Estilo para o input
+        value={text}           // O valor do input vem do estado 'text'
+        onChangeText={setText} // Atualiza o estado 'text' com o que o usuário digitar
+        placeholder="Digite aqui . . ." // Texto sugerido quando o campo está vazio
+      />
+      </View>
+      <Button
+      style={styles.button_salvar}
+      color="#FFAF32"
+      title='Salvar'
+      onPress = {adicionar_alarme}
+      />
+      
+      {/* Exibir o que o usuário digitou */}
         </View>)}
 
         <View style={styles.navBar}>
@@ -171,7 +297,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#E5E5E5'
+    backgroundColor: '#E6E6D6'
   },
   navigationContainer: {
     backgroundColor: '#ecf0f1',
@@ -191,6 +317,101 @@ const styles = StyleSheet.create({
     shadowRadius: 10, // Raio da sombra
     // alignItems: 'center',
   },
+  parent: {
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    width: '100%',
+  },
+  rectangle: {
+    // height: '5%',        // 25% da altura do pai
+    width: '95%',         // 95% da largura do pai
+    backgroundColor: '#FF8066',  // Cor de fundo do retângulo
+    marginBottom: 15,     // Espaço entre os retângulos
+    marginLeft: "2.5%",
+    borderRadius: 15,      // Bordas arredondadas
+  },
+  text_alarm: {
+    color: "#fff",
+    fontSize: 25,
+    padding: 15,
+    paddingBottom: 0
+    // height: "10%"
+  },
+  text_alarm_hora: {
+    color: "#fff",
+    fontSize: 30,
+    padding: 20,
+    paddingTop: 0
+    // height: "10%"
+  },
+  button_input: {
+    backgroundColor: '#FF8066',
+    paddingVertical: 15,
+    height: "30%",
+    paddingHorizontal: 25,
+    marginVertical: 10,
+    borderRadius: 15,
+    width: '95%', // Definindo a largura dos botões
+    // justifyContent: 'center',
+    elevation: 5,
+    shadowColor: '#000', // Cor da sombra (preto)
+    shadowOffset: { width: 0, height: 4 }, // Distância da sombra
+    shadowOpacity: 0, // Opacidade da sombra
+    shadowRadius: 10, // Raio da sombra
+    // alignItems: 'center',
+  },
+  text_input:{
+    marginBottom: 15,
+    color: '#Fff',
+    paddingBottom: 10,
+    width: "100%",
+    borderBottomWidth: 1,
+    borderColor: '#fff'
+
+  },
+  button_salvar: {
+    color: '#FFAF32'
+  },
+  appName: {
+		fontSize: 28,
+		// fontWeight: "bold",
+		color: "#FFAF32", // Set your desired text color
+	},
+  clockContainer: {
+		flexDirection: "row",
+		alignItems: "center",
+		marginBottom: 20,
+	},
+  clockText: {
+		fontSize: 80,
+		marginRight: 10,
+    borderBottomWidth: 3,
+    borderColor: "#FF765A",
+		color: "#fff", // Set your desired text color
+    padding: 35,
+    paddingBottom: 10,
+    textShadowColor: '#778899',    // Cor da sombra
+    textShadowOffset: {         // Deslocamento da sombra
+      width: 2,  // Deslocamento horizontal (x)
+      height: 3, // Deslocamento vertical (y)
+    },
+    textShadowRadius: 3, 
+	},
+  input: {
+    color: '#fff',
+    width: '100%',
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingLeft: 8,
+    marginBottom: 16,
+  },
+  text: {
+    fontSize: 16,
+    marginTop: 8,
+  },
   buttonText: {
     color: 'white',
     fontSize: 16,
@@ -200,7 +421,30 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     flex: 0.8,
     // borderWidth: 3,
-    borderColor: 'red',
+    borderColor: '#FF8167',
+    marginBottom: 50
+  },
+  box1: {
+    width: '100%',
+    // alignItems: "center",
+    // borderWidth: 3,
+    height: "100%",
+    borderColor: '#FF8167',
+    // marginBottom: 50
+  },
+  box_mais: {
+    position: 'absolute',
+    top: height * 0.15,
+    width: width * 1,
+    height: height * 0.6,
+    alignItems: 'center',
+    flex: 0.6,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    borderBottomRightRadius: 10,
+    borderBottomLeftRadius: 10,
+    borderWidth: 3,
+    borderColor: '#FF8167',
     marginBottom: 50
   },
   paragraph: {
